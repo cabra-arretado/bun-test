2bb0acfb00b2ba2d17ebc7ddc6908a2f7906d062
Felipe <felipemouradev@gmail.com>
2023-09-10 21:26:21 -0500

    figlet endpoint

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,8 @@ import figlet from "figlet";
 
 export default function router(request: Request) {
   switch (get_path(request)) {
-    case "/bun":
-      return new Response(figlet.textSync("Hello"))
+    case "/figlet_me":
+      return figlet_me(request);
     default:
       return new Response("Welcome to Bun!");
 }
@@ -11,4 +11,17 @@ export default function router(request: Request) {
 
 function get_path(request: Request) {
   return new URL(request.url).pathname;
+}
+
+async function figlet_me(request: Request) {
+  if (request.method == "GET") {
+    return new Response(figlet.textSync("Hello World!"));
+  }
+  if (request.method == "POST") {
+    let body = await request.text();
+    body = JSON.parse(body);
+    console.log(body);
+    return new Response(figlet.textSync(body));
+  }
+  return new Response("Method not allowed", { status: 405 });
 }
