b8ac81d5e7f0a673c4a29a756e4f2d08a0917414
Felipe <felipemouradev@gmail.com>
2023-09-11 13:33:54 -0500

    polishing

---
+++ src/endpoints/figlet_me.ts
@@ -0,0 +1,16 @@
+import figlet from "figlet";
+import Endpoint from "endpoints/class";
+
+class FigletMe extends Endpoint {
+  constructor() {
+    super("/figlet_me", ["GET", "POST"]);
+  }
+  async get(request: Request) {
+    return new Response(figlet.textSync("Hello World!"));
+  }
+  async post(request: Request) {
+    let body = await request.text();
+    console.log(body);
+    return new Response(figlet.textSync(body));
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,20 @@
 import figlet from "figlet";
+import Endpoint from "endpoints/class";
+
+
+let ENDPOINTS: Endpoint[] = [];
+
 
 export default function router(request: Request) {
-  switch (get_path(request)) {
-    case "/figlet_me":
-      return figlet_me(request);
-    default:
-      return new Response("Welcome to Bun!");
-}
+  let path = get_path(request);
+  for (let endpoint of ENDPOINTS) {
+    if (endpoint.path == path) {
+      return endpoint.run(request);
+    }
+  }
+  return new Response("Not found", { status: 404 });
 }
 
 function get_path(request: Request) {
   return new URL(request.url).pathname;
-}
-
-async function figlet_me(request: Request) {
-  if (request.method == "GET") {
-    return new Response(figlet.textSync("Hello World!"));
-  }
-  if (request.method == "POST") {
-    let body = await request.text();
-    console.log(body);
-    return new Response(figlet.textSync(body));
-  }
-  return new Response("Method not allowed", { status: 405 });
 }
